3f5c2d021f7a9e805dac8274a3ced56b79a2b39a
Travis Southard <tsouthard88@gmail.com>
2021-02-05 10:01:20 -0500

    Restarting with just a basic working snake. Deleted all the files and starting from scratch

--- sketch.js
+++ sketch.js
@@ -1,30 +1,7 @@
-let rez = 40;
-let snake;
-
 function setup() {
-  frameRate(2);
-  createCanvas(400, 400);
-  snake = new Snake();
-}
-
-function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    snake.vel.x = rez * -1;
-    snake.vel.y = 0;
-  } else if (keyCode === RIGHT_ARROW) {
-    snake.vel.x = rez;
-    snake.vel.y = 0;
-  } else if (keyCode === UP_ARROW) {
-    snake.vel.x = 0;
-    snake.vel.y = rez * -1;
-  } else if (keyCode === DOWN_ARROW) {
-    snake.vel.x = 0;
-    snake.vel.y = rez;
-  }
+    createCanvas(400, 400);
 }
 
 function draw() {
-  background(0, 100, 100);
-  snake.update();
-  snake.show();
+    background(220);
 }
